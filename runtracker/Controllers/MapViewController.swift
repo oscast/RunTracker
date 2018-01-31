7dd31d621bc68cee7f9e6fc9418bb0c3223b8c1c
Oscar Castillo Villacorta <goldoac@live.com>
2018-01-31 15:39:10 -0600

    Added viewControllers for views

---
+++ runtracker/Controllers/MapViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MapViewController.swift
+//  runtracker
+//
+//  Created by Oscar Castillo Villacorta on 1/31/18.
+//  Copyright © 2018 nextu. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ runtracker/Controllers/RouteTrackerViewController.swift
@@ -0,0 +1,57 @@
+//
+//  RouteTrackerViewController.swift
+//  runtracker
+//
+//  Created by Oscar Castillo Villacorta on 1/31/18.
+//  Copyright © 2018 nextu. All rights reserved.
+//
+
+import UIKit
+
+class RouteTrackerViewController: UIViewController {
+
+    @IBOutlet weak var musicButton: UIButton!
+    @IBOutlet weak var routeRegisterButton: UIButton!
+    @IBOutlet weak var routeDrawButton: UIButton!
+    @IBOutlet weak var musicStatusLabel: UILabel!
+    @IBOutlet weak var routeRegisterStatusLabel: UILabel!
+    @IBOutlet weak var routeDrawStatusLabel: UILabel!
+    
+    @IBOutlet weak var routeStartButton: UIButton!
+    @IBOutlet weak var closeButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func CloseAction(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func musicAction(_ sender: UIButton) {
+        
+    }
+   
+    @IBAction func routeRegisterAction(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func routeDrawAction(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func startAction(_ sender: UIButton) {
+        
+    }
+
+}
